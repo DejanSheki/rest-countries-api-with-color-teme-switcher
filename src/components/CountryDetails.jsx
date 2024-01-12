9151bd75e387af0f50f84b3918484b96f7272fcf
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-01-12 20:25:08 +0100

    styling

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -7,63 +7,66 @@ const CountryDetails = ({ data }) => {
     const { alpha3Code } = useParams();
 
     return (
-        <section className="flex flex-col dark:bg-veryDarkBlueBg bg-veryLightGrayBg p-15 lg:px-20 h-[91vh]">
+        <section className="flex flex-col dark:bg-veryDarkBlueBg bg-veryLightGrayBg px-6 sm:px-12 lg:px-20 pb-20 md:h-[91vh]">
             {data.filter((countryData) => countryData.alpha3Code === alpha3Code).map((country) => {
                 return (
                     <div key={country.numericCode}>
-                        <div className="flex justify-start items-center h-[30vh] dark:bg-veryDarkBlueBg bg-veryLightGrayBg">
-                            <Link to='/' className="dark:bg-darkBlue bg-white px-8 py-4 border-none rounded-md flex items-center text-veryDarkBlue dark:text-white tracking-wide font-light dark:shadow-[0_0_7px_rgba(17,21,23,0.8)] shadow-[0_0_7px_rgba(17,21,23,0.2)">
+                        <div className="flex justify-start items-center pt-10 pb-12 md:py-16 dark:bg-veryDarkBlueBg bg-veryLightGrayBg">
+                            <Link to='/' className="dark:bg-darkBlue bg-white px-9 py-3 border-none rounded-md flex items-center text-veryDarkBlue dark:text-white tracking-wide font-light dark:shadow-[0_0_7px_rgba(17,21,23,0.8)] shadow-[0_0_7px_rgba(17,21,23,0.2)]">
                                 <FaArrowLeftLong size={18} type="button" className="stroke-1" /><span className="ml-3"> Back</span>
                             </Link>
                         </div>
-                        <div className="flex justify-between items-center ">
-                            <div className="w-2/4">
+                        <div className="flex flex-col md:flex-row justify-center md:justify-between items-center">
+                            <div className="flex flex-wrap w-full md:w-[40%]">
                                 <img src={country.flag} alt={`${country.name} flag`} />
                             </div>
-                            <div>
-                                <div>
-                                    <h1>{country.name}</h1>
+                            <div className="w-full md:w-2/4">
+                                <h1 className="dark:text-white text-veryDarkBlue font-extrabold text-2xl sm:text-3xl tracking-wide sm:my-8 my-5">{country.name}</h1>
+                                <div className="flex flex-col md:flex-row items-start justify-between">
                                     <div>
-                                        <p><strong>Native Name:</strong> {country.name}</p>
-                                        <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
-                                        <p><strong>Region:</strong> {country.region}</p>
-                                        <p><strong>Sub Region:</strong> {country.subregion}</p>
-                                        <p><strong>Capital:</strong> {country.capital}</p>
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Native Name:</strong> {country.name}</p>
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Population:</strong> {country.population.toLocaleString()}</p>
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Region:</strong> {country.region}</p>
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Sub Region:</strong> {country.subregion}</p>
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Capital:</strong> {country.capital}</p>
                                     </div>
-                                    <div>
-                                        <p><strong>Top Level Domain</strong> {country.topLevelDomain}</p>
-                                        <p><strong>Currencies:</strong>
+                                    <div className="w-2/5">
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Top Level Domain</strong> {country.topLevelDomain}</p>
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Currencies: </strong>
                                             {country.currencies ? (
                                                 country.currencies.map((currency, index) => (
                                                     <span key={index}>
-                                                        {currency.name}, <br /> Code: {currency.code}, <br /> Symbol: {currency.symbol}</span>
+                                                        {currency.name}  {currency.symbol}</span>
                                                 ))
                                             ) : (
                                                 <span> No data</span>
                                             )}
                                         </p>
-                                        <p><strong>Languages:</strong>
+                                        <p className="dark:text-white text-veryDarkBlue tracking-wide font-light"><strong className="font-semibold">Languages:</strong>
                                             {country.languages ? (
                                                 country.languages.map((language, index) => (
                                                     <span key={index}> {(index ? ', ' : '') + language.name} </span>
                                                 ))
                                             ) : (
-                                                <span> No land borders</span>
+                                                <span className="font-bold"> No data</span>
                                             )}
                                         </p>
                                     </div>
                                 </div>
-                                <div className="flex">
-                                    <strong>Border Cuntries:</strong>
-                                    {country.borders ? (
-                                        country.borders.map((border, index) => (
-                                            <button key={index} className="dark:bg-darkBlue bg-white px-8 py-4 border-none rounded-md flex items-center text-veryDarkBlue dark:text-white tracking-wide font-light dark:shadow-[0_0_7px_rgba(17,21,23,0.8)] shadow-[0_0_7px_rgba(17,21,23,0.2)">
-                                                {border}
-                                            </button>
-                                        ))
-                                    ) : (
-                                        <span> No Data</span>
-                                    )}
+                                <div className="sm:inline-flex flex sm:flex-row flex-col sm:items-center mt-6">
+                                    <strong className="dark:text-white text-veryDarkBlue tracking-wide font-semibold">Border Cuntries:</strong>
+                                    <div className="flex items-center gap-2 flex-wrap sm:ml-3">
+                                        {country.borders ? (
+                                            country.borders.map((border, index) => (
+                                                <Link to={'/'} key={index} className="dark:bg-darkBlue bg-white px-6 py-1 border-none rounded-md flex items-center text-veryDarkBlue dark:text-white tracking-wide font-light dark:shadow-[0_0_7px_rgba(17,21,23,0.8)] shadow-[0_0_7px_rgba(17,21,23,0.2)]">
+                                                    {border}
+                                                </Link>
+                                            ))
+                                        ) : (
+                                            <span className="dark:text-white text-veryDarkBlue tracking-wide font-semibold"> No land borders</span>
+                                        )}
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
